493694de17b5dc5228cb8bae83f4a07b42c24817
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2021-02-02 17:47:00 +0200

    first commit

---
+++ envault.rb
@@ -0,0 +1,13 @@
+class Envault < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kobibarhanin/envault/archive/v0.0.1.tar.gz"
+  sha256 "de95df93afe4841c6cacb088c38de195fb9261bd45112f7177f3b5b9310c4e76"
+  license ""
+
+
+  def install
+      bin.install "envault"
+  end
+
+end
